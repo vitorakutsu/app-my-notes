bbb2cb8fa8713104288a09a8a95b52799c0af371
vitorakutsu <vitorakutsu@gmail.com>
2024-09-12 01:54:23 -0300

    feat: updated MainActivity

--- app/src/main/java/br/com/mynotes/MainActivity.java
+++ app/src/main/java/br/com/mynotes/MainActivity.java
@@ -1,14 +1,87 @@
 package br.com.mynotes;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.mynotes.adapters.NoteAdapter;
+import br.com.mynotes.entities.Note;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private NoteAdapter noteAdapter;
+    private List<Note> notes = new ArrayList<>();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.newNote) {
+            Toast.makeText(this, "Criar nova anotação", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        if(item.getItemId() == R.id.sortPriority) {
+            Toast.makeText(this, "Ordenar por prioridade", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        if(item.getItemId() == R.id.sortOrder) {
+            Toast.makeText(this, "Ordenar por ordem", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        MenuInflater inflater = popupMenu.getMenuInflater();
+        inflater.inflate(R.menu.menu, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.id.newNote) {
+                    return true;
+                }
+
+                if(item.getItemId() == R.id.sortPriority) {
+                    return true;
+                }
+
+                if(item.getItemId() == R.id.sortOrder) {
+                    return true;
+                }
+
+                return false;
+            }
+        });
+        popupMenu.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +93,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageButton imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(v);
+            }
+        });
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new NoteAdapter(this, notes);
+        recyclerView.setAdapter(noteAdapter);
+
+        loadNotes();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void loadNotes() {
+        notes.add(new Note("Note 1", "This is the note text.", "low"));
+        notes.add(new Note("Note 2", "This is the note text.", "medium"));
+        notes.add(new Note("Note 3", "This is the note text.", "high"));
+
+        noteAdapter.notifyDataSetChanged();
     }
 }
